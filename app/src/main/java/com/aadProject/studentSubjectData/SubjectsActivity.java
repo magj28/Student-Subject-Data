e9f9ce87a81100a79199a760ee59678ed0015dcb
Swarnim Modi <modiswarnim@gmail.com>
2022-10-10 20:22:13 +0530

    just item_batchmate left to make

---
+++ app/src/main/java/com/aadProject/studentSubjectData/SubjectsActivity.java
@@ -0,0 +1,14 @@
+package com.aadProject.studentSubjectData;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubjectsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.subjects);
+    }
+}
